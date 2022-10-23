042bbfbeb9f514fe2dc4c2a0237267d9fbb3760e
rizzgo <seipel.richard@gmail.com>
2022-10-23 13:08:53 +0200

    Link update

--- models/link.py
+++ models/link.py
@@ -2,29 +2,25 @@ from pymongo import database
 
 # Structure :
 
-#   station : {
-#       _id :           ID,
-#       locationId:     ID,
-#       links:          [
+#   link : {
 #           _id:                    ID,
 #           trainType:              int,
 #           trafficType:            int,
 #           commercialTrafficType:  int,
 #           operationalTrainNumber: int,
 #           
-#           palannedCalendar:        [
-#               date:   Date
+#           plannedCalendar:        [
+#              date:   Date
 #           ]
-#       ]
 #   }
 
-class StationModel:
+class LinkModel:
     
     def __init__(self, db : database.Database):
         self.db = db
         
     def insert(self, name, id, countryCode):
-        collection = self.db['station']
+        collection = self.db['link']
         collection.insert_one({
             
         })
