b3437ff5779c908993f1d37b9629d2d4abfc8597
rizzgo <seipel.richard@gmail.com>
2022-10-24 13:03:28 +0200

    Add calendar

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 import zipfile
 import os
@@ -8,25 +8,26 @@ from database import Database
 
 
 class Parser:
-    timeFormat = '%H:%M:%S.0000000%z'
+    station_time_format = '%H:%M:%S.0000000%z'
+    calendar_datetime_format = '%Y-%m-%dT%H:%M:%S'
     
     def __init__(self, db: Database, timetable: str = "GVD2022"):
         self.db = db
         self.timetable = timetable
-        self.__xmlDIR = f"temp/{timetable}"
+        self.__xml_dir = f"temp/{timetable}"
 
     def __download(self):
         if not os.path.isdir('temp/'):
             os.makedirs('temp/')
 
-        if not os.path.isdir(self.__xmlDIR):
+        if not os.path.isdir(self.__xml_dir):
             url = f"https://portal.cisjr.cz/pub/draha/celostatni/szdc/2022/{self.timetable}.zip"
-            request.urlretrieve(url, f"{self.__xmlDIR}.zip")
+            request.urlretrieve(url, f"{self.__xml_dir}.zip")
 
-            with zipfile.ZipFile(f"{self.__xmlDIR}.zip","r") as zip_ref:
-                zip_ref.extractall(self.__xmlDIR)
+            with zipfile.ZipFile(f"{self.__xml_dir}.zip","r") as zip_ref:
+                zip_ref.extractall(self.__xml_dir)
 
-    def __parse_stations(self, stations: list[ET.Element]):
+    def __parse_stations(self, stations: list[ET.Element]) -> list[link_models.linkStationStructure]:
         parsed_stations = []
         for station in stations:
             train_activities = [link_models.trainActivityStructure(
@@ -43,25 +44,38 @@ class Parser:
                 )
             ) for activity in station.findall("./TrainActivity")]
 
-            locationId = station.find("./Location/LocationPrimaryCode")
-            locationId = locationId.text if locationId != None else None
+            location_id = station.find("./Location/LocationPrimaryCode")
+            location_id = location_id.text if location_id != None else None
 
-            arrivalTime = station.find("./TimingAtLocation/Timing[@TimingQualifierCode='ALA']/Time")
-            arrivalTime = datetime.strptime(arrivalTime.text, self.timeFormat) if arrivalTime != None and arrivalTime.text != None else None
+            arrival_time = station.find("./TimingAtLocation/Timing[@TimingQualifierCode='ALA']/Time")
+            arrival_time = datetime.strptime(arrival_time.text, self.station_time_format) if arrival_time != None and arrival_time.text != None else None
             
-            departureTime = station.find("./TimingAtLocation/Timing[@TimingQualifierCode='ALD']/Time")
-            departureTime = datetime.strptime(departureTime.text, self.timeFormat) if departureTime != None and departureTime.text != None else None
+            departure_time = station.find("./TimingAtLocation/Timing[@TimingQualifierCode='ALD']/Time")
+            departure_time = datetime.strptime(departure_time.text, self.station_time_format) if departure_time != None and departure_time.text != None else None
 
-            parsed_stations.append(link_models.linkStationStructure(locationId, arrivalTime, departureTime, train_activities))
+            parsed_stations.append(link_models.linkStationStructure(location_id, arrival_time, departure_time, train_activities))
             
         return parsed_stations
+
+    def __parse_calendar(self, calendar: ET.Element) -> list[datetime]:
+        parsed_calendar = []
+        bitmap_days = calendar.find("./BitmapDays").text
+        start_date = datetime.strptime(calendar.find("./ValidityPeriod/StartDateTime").text, self.calendar_datetime_format)
+        end_date = datetime.strptime(calendar.find("./ValidityPeriod/EndDateTime").text, self.calendar_datetime_format)
+
+        day_offset = 0
+        for day in bitmap_days:
+            if day == "1":
+                parsed_calendar.append(start_date+timedelta(days=day_offset))
+            day_offset+=1
+        return parsed_calendar
 
     def parse(self):
         self.__download()
         
-        for file in os.listdir(self.__xmlDIR):
+        for file in os.listdir(self.__xml_dir):
             print(file)
-            file_path = os.path.join(self.__xmlDIR, file)
+            file_path = os.path.join(self.__xml_dir, file)
             tree = ET.parse(file_path)
             root = tree.getroot()
 
@@ -69,12 +83,13 @@ class Parser:
             tr_identifier = root.find("./Identifiers/PlannedTransportIdentifiers/[ObjectType='TR']")
             path_info = root.find("./CZPTTInformation")
             stations = path_info.findall("./CZPTTLocation")
-            calendar = path_info.findall("./PlannedCalendar")
+            calendar = path_info.find("./PlannedCalendar")
 
             pa_id = "_".join(element.text for element in pa_identifier)
             tr_id = "_".join(element.text for element in tr_identifier)
             link_id = pa_id + "__" + tr_id
             
             parsed_stations = self.__parse_stations(stations)
+            parsed_calendar = self.__parse_calendar(calendar)
 
-            self.db.linkModel.insert(id=link_id, stations = parsed_stations, calendar=[datetime.now()])
+            self.db.linkModel.insert(id=link_id, stations=parsed_stations, calendar=parsed_calendar)
